2bee28aa79cc96172e0b391ff68d9c0fa20c0fa7
Alex-Prokhorenko <galexthunder@gmail.com>
2022-11-26 14:49:56 +0700

    add new 4 codewars 6kyu challenge

---
+++ js/6kyu/replace-with-alphabet-position.js
@@ -0,0 +1,19 @@
+/*
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+Example
+
+alphabetPosition("The sunset sets at twelve o' clock.")
+
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+*/
+
+// My solution
+function alphabetPosition(text) {
+  let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  return [...text.toLowerCase()].map(letter => (arr.indexOf(letter) + 1)).filter(elem => elem > 0).join(' ');
+}
