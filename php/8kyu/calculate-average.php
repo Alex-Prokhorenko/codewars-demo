edfab54e0431a98dfa589d6f75786e7b28434db1
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-07 15:26:31 +0700

    add 2 new codewars php 8kyu challenges

---
+++ php/8kyu/calculate-average.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+// My solution
+
+function find_average($array): float {
+    return empty($array) ? 0 : array_sum($array) / count($array);
+}
