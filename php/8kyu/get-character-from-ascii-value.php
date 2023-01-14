7f4b0d437edbeeef242bf32f7f903356af48748b
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-14 17:21:08 +0700

    add 3 new codewars php 8kyu challenge

---
+++ php/8kyu/get-character-from-ascii-value.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Write a function get_char() / getChar() which takes a number
+and returns the corresponding ASCII char for that value.
+*/
+
+// My solution
+
+function getChar($c)
+{
+    return chr($c);
+}
