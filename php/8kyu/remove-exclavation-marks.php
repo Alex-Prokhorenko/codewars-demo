a8f04e540d47f9afd39e75d637ab0ffacd8a4da9
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-20 17:00:44 +0700

    add 2 new codewars php 8kyu challenges

---
+++ php/8kyu/remove-exclavation-marks.php
@@ -0,0 +1,10 @@
+<?php
+//Description:
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// My solution
+
+function remove_exclamation_marks($string)
+{
+    return str_replace('!', '', $string);
+}
