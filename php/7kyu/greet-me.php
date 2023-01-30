2f57b62fb096c26a795eaeb1f470ab38e25c9f45
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-30 15:00:37 +0700

    add new codewars php 7kyu challenge

---
+++ php/7kyu/greet-me.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Write a method that takes one argument as name and then greets that name,
+capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
+// My solution
+
+function greet($name)
+{
+    return "Hello " . ucfirst(strtolower($name)) . "!";
+}
